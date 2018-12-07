9ae986138d1e7c2901a95d5f6c518b0ce388c5ab
Hans W. Uhlig <hans.uhlig@ibm.com>
2018-12-07 09:54:01 -0800

    Adding Rave Mode

--- src/main.rs
+++ src/main.rs
@@ -15,25 +15,60 @@
 //
 
 use clap::{App, Arg, SubCommand};
+use rand::{thread_rng, Rng};
 use std::io::Write;
 use std::net::TcpStream;
+use std::time::Duration;
+use std::thread::sleep;
+
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8) -> Color {
+        Color { r, g, b }
+    }
+    pub fn black() -> Color {
+        Color { r: 0, g: 0, b: 0 }
+    }
+    pub fn red() -> Color {
+        Color { r: 255, g: 0, b: 0 }
+    }
+    pub fn green() -> Color {
+        Color { r: 0, g: 255, b: 0 }
+    }
+    pub fn blue() -> Color {
+        Color { r: 0, g: 0, b: 255 }
+    }
+    pub fn yellow() -> Color {
+        Color { r: 255, g: 255, b: 0 }
+    }
+    pub fn cyan() -> Color {
+        Color { r: 0, g: 255, b: 255 }
+    }
+    pub fn magenta() -> Color {
+        Color { r: 255, g: 0, b: 255 }
+    }
+    pub fn white() -> Color {
+        Color { r: 255, g: 255, b: 255 }
+    }
+}
 
 enum Command {
     On,
     Off,
-    Color {
-        r: u8,
-        g: u8,
-        b: u8,
-    },
+    Color(Color),
 }
 
 impl Command {
     fn to_buffer(&self) -> Vec<u8> {
-        let mut buffer = match *self {
+        let mut buffer = match self {
             Command::On => vec![0x71, 0x23, 0x0f],
             Command::Off => vec![0x71, 0x24, 0x0f],
-            Command::Color { r, g, b } => vec![0x31, r, g, b, 0x00, 0x00, 0x0f],
+            Command::Color(color) => vec![0x31, color.r, color.g, color.b, 0x00, 0x00, 0x0f],
         };
         let checksum: u8 = buffer.iter().sum::<u8>() & 0xFF;
         buffer.push(checksum);
@@ -74,27 +109,47 @@ fn main() {
         .subcommand(SubCommand::with_name("off")
             .about("Turn Strip Off")
         )
+        .subcommand(SubCommand::with_name("rave")
+            .about("LED Rave Mode")
+            .arg(Arg::with_name("time")
+                .help("Set Rave Time Delay")
+                .short("t")
+                .long("time")
+                .required(true)
+                .default_value("200")
+            )
+        )
         .subcommand(SubCommand::with_name("color")
             .about("Set Strip Color")
+            .arg(Arg::with_name("name")
+                .help("Set Color By Name")
+                .short("n")
+                .long("name")
+                .takes_value(true)
+                .required(false)
+                .conflicts_with("red")
+                .conflicts_with("green")
+                .conflicts_with("blue")
+            )
             .arg(Arg::with_name("red")
                 .help("Set Red Value")
                 .short("r")
                 .long("red")
-                .required(true)
+                .required_unless("name")
                 .default_value("0")
             )
             .arg(Arg::with_name("green")
                 .help("Set Green Value")
                 .short("g")
                 .long("green")
-                .required(true)
+                .required_unless("name")
                 .default_value("0")
             )
             .arg(Arg::with_name("blue")
                 .help("Set Blue Value")
                 .short("b")
                 .long("blue")
-                .required(true)
+                .required_unless("name")
                 .default_value("0")
             )
         )
@@ -106,11 +161,44 @@ fn main() {
     match matches.subcommand() {
         ("on", Some(_)) => send_command(host, port, Command::On),
         ("off", Some(_)) => send_command(host, port, Command::Off),
-        ("color", Some(matches)) => send_command(host, port, Command::Color {
-            r: matches.value_of("red").unwrap().parse().expect("Unable to parse red value"),
-            g: matches.value_of("green").unwrap().parse().expect("Unable to parse green value"),
-            b: matches.value_of("blue").unwrap().parse().expect("Unable to parse blue value"),
-        }),
+        ("rave", Some(matches)) => {
+            let time = matches.value_of("time").unwrap()
+                .parse().expect("Unable to parse time value");
+            let mut rng = thread_rng();
+            loop {
+                send_command(host, port, Command::Color(Color::new(
+                    rng.gen_range(0, 255),
+                    rng.gen_range(0, 255),
+                    rng.gen_range(0, 255),
+                ))).unwrap();
+                sleep(Duration::from_millis(time));
+            }
+        }
+        ("color", Some(matches)) => send_command(host, port, Command::Color(
+            if matches.is_present("name") {
+                match matches.value_of("name") {
+                    Some("black") => Color::black(),
+                    Some("red") => Color::red(),
+                    Some("green") => Color::green(),
+                    Some("blue") => Color::blue(),
+                    Some("yellow") => Color::yellow(),
+                    Some("cyan") => Color::cyan(),
+                    Some("magenta") => Color::magenta(),
+                    Some("white") => Color::white(),
+                    Some(color) => panic!("Unknown Color {}", color),
+                    None => panic!("Color not specified"),
+                }
+            } else {
+                Color::new(
+                    matches.value_of("red").unwrap()
+                        .parse().expect("Unable to parse red value"),
+                    matches.value_of("green").unwrap()
+                        .parse().expect("Unable to parse green value"),
+                    matches.value_of("blue").unwrap()
+                        .parse().expect("Unable to parse blue value"),
+                )
+            }
+        )),
         _ => Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "No subcommand was used.")),
     }.unwrap()
 }
